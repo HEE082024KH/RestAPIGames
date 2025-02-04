d9c3ce4aa551e644b4a784121455fbc42c135515
HEE082024KH <hee082024@gmail.com>
2025-02-04 14:05:36 +0100

    Added database connections

---
+++ Controller/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPIGames.Model;
+
+namespace RestAPIGames.Controller;
+
+public class AppDbContext : DbContext
+{
+  public DbSet<Games> Games { get; set; }
+  
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
